c815f0039d21c5e0207c821dbbdeb9629ebefaea
jg185252 <jg185252@WINJG185252-CMQ.corp.ncr.com>
2017-09-28 08:43:06 +0530

    My First Assignment- Printing Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {     //Class Name defined
+
+	public static void main (String[] args) //Public is access specifier means is accessible publically. 
+											//Static is access modifier, void is return type and main is the method
+	{
+		System.out.println("Hello World!");  //Print the output on console.
+	}
+	
+}
